634fdeee18f5694e7242480b7b01b391bb419430
@Tacaa <t.aca220799@hotmail.com>
2024-07-27 21:11:27 +0200

    feat: Setup frontend with routing and angular material

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule  } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AngularMaterialModule } from './angular_material.module';
+import { HomepageComponent } from './homepage/homepage.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomepageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AngularMaterialModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [
+  
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
